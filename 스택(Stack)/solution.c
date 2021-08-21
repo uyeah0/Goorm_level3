14be9699aba468ddec607e3b7ff807368b09ef19
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-08-21 23:28:52 +0900

    Update solution.c

--- 스택(Stack)/solution.c
+++ 스택(Stack)/solution.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #define MAX 10
-void push();
-void pop();
-int top = -1;
 int stack[10] = {0,};
+int top=-1;
 
-int main() {
-	int n,data;
-	scanf("%d",&n);
-	for(int i = 0; i < n; i++){
-		scanf("%d",&data);
-			if(data==0)push();
-			else if(data==1) pop();
-			else break;
+int push(int n){
+	if(MAX -1 == top){
+		printf("overflow\n");
+		return 0;
 	}
-	
-	if(top != -1){
-		for(int i = 0; i <= top; i++){
-			printf("%d ",stack[i]);	
-		}	
-	}
+	stack[++top] = n;
 	return 0;
 }
-void push(){
-	int d;
-	scanf("%d", &d);
-	top++;
-	if(top==MAX){
-		printf("overflow\n");
-		top--;
-	}else{
-		stack[top] = d;
-	}
-}
-void pop(){
-	if(top==-1){
+int pop(){
+	if(top == -1){
 		printf("underflow\n");
-	}else{
-		printf("%d\n",stack[top]);
-		top--;
+		return 0;
 	}
+	top--;
+	return 0;
+}
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	for(int i =0; i < a; i++){
+		int n;
+		scanf("%d", &n);
+		if(n == 1){ pop(); }
+		else if( n == 0) { 
+			int num;
+			scanf("%d", &num);
+			push(num);	
+		}else{
+			break;
+		}
+	}
+	
+	for(int i =0; stack[i] !=0; i++){
+		printf("%d ", stack[i]);
+	}
+
+		return 0;
 }
