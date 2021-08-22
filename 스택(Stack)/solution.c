391dd2ddfb77eb9215852e44738b1644aea19116
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-08-22 23:03:43 +0900

    Update solution.c

--- 스택(Stack)/solution.c
+++ 스택(Stack)/solution.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #define MAX 10
-int stack[10] = {0,};
+int stack[MAX] = {0,};
 int top=-1;
 
 int push(int n){
-	if(MAX -1 == top){
+	if(MAX - 1 == top){
 		printf("overflow\n");
-		return 0;
+		return -1;
 	}
 	stack[++top] = n;
 	return 0;
@@ -14,7 +14,7 @@ int push(int n){
 int pop(){
 	if(top == -1){
 		printf("underflow\n");
-		return 0;
+		return -1;
 	}
 	top--;
 	return 0;
@@ -26,13 +26,17 @@ int main() {
 	for(int i =0; i < a; i++){
 		int n;
 		scanf("%d", &n);
-		if(n == 1){ pop(); }
-		else if( n == 0) { 
+		if(n != 1 && n != 0) {
+			break;
+		}
+		
+		if(n == 0){ 
 			int num;
 			scanf("%d", &num);
 			push(num);	
-		}else{
-			break;
+		}
+		else{ 
+			pop();
 		}
 	}
 	
